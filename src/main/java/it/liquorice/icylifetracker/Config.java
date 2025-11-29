b67d82429e9041275118a86b63c9a9ba29ed3cdd
liquorice-byte <liquorice-byte@outlook.com>
2025-11-29 11:48:35 +0800

    feat. Add config

--- src/main/java/it/liquorice/icylifetracker/Config.java
+++ src/main/java/it/liquorice/icylifetracker/Config.java
@@ -7,7 +7,9 @@ import net.neoforged.neoforge.common.ModConfigSpec;
 public class Config {
     private static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
 
-
+    public static final ModConfigSpec.IntValue WARNING_THRESHOLD = BUILDER
+            .comment("Trigger warning when the health is less than or equal to this value")
+            .defineInRange("warningThreshold", 20, 1, Integer.MAX_VALUE);
 
     static final ModConfigSpec SPEC = BUILDER.build();
 
